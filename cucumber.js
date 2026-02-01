26ed4a50e646488d7b977f62323132601e20698e
sandeepay27 <sandeepay.20@uom.lk>
2026-02-01 17:57:06 +0530

    update-cicd

--- cucumber.js
+++ cucumber.js
@@ -4,5 +4,25 @@ module.exports = {
         requireModule: ['ts-node/register'],
         require: ['src/steps/**/*.ts', 'src/support/**/*.ts'],
         format: ['progress', 'html:reports/cucumber-report.html']
+    },
+    ui: {
+        paths: ['src/features/**/*.feature'],
+        requireModule: ['ts-node/register'],
+        require: ['src/steps/**/*.ts', 'src/support/**/*.ts'],
+        tags: '@UI',
+        format: ['progress', 'html:reports/ui-report.html']
+    },
+    api: {
+        paths: ['src/features/**/*.feature'],
+        requireModule: ['ts-node/register'],
+        require: ['src/steps/**/*.ts', 'src/support/**/*.ts'],
+        tags: '@API',
+        format: ['progress', 'html:reports/api-report.html']
+    },
+    full: {
+        paths: ['src/features/**/*.feature'],
+        requireModule: ['ts-node/register'],
+        require: ['src/steps/**/*.ts', 'src/support/**/*.ts'],
+        format: ['progress', 'html:reports/full-report.html', 'json:reports/report.json']
     }
 }
